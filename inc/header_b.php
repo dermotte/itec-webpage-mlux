ae5f6cf0be1d648944346f20b38a94f89ad5eaeb
dermotte <mathias@juggle.at>
2015-02-27 11:42:05 +0100

    updated bootstrap, changed font, updated games course web page

--- inc/header_b.php
+++ inc/header_b.php
@@ -9,15 +9,17 @@
 
     <title>Prof. Dr. Mathias Lux</title>
 
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 <!--    <link rel="stylesheet" href="http://bootswatch.com/superhero/bootstrap.min.css">-->
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script></head>
+    <script src="bootstrap/js/bootstrap.min.js"></script></head>
 
 <body class="">
 <?php

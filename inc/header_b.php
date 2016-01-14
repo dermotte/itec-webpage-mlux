435441baf9a4baa0cd2c1035cd7e58591a38e867
Mathias Lux <mathias.lux@aau.at>
2016-01-14 13:18:27 +0100

    identity-gen

--- inc/header_b.php
+++ inc/header_b.php
@@ -23,6 +23,9 @@
     <!-- Latest compiled and minified JavaScript -->
     <script src="bootstrap/js/bootstrap.min.js"></script></head>
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
 <body class="">
 <?php
 if (array_key_exists("id", $_GET)) {

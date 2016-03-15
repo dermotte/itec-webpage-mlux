2e02d51d63e465f136116d761d669e6bd5179c0b
Mathias Lux <mathias.lux@aau.at>
2016-03-15 17:13:01 +0100

    added post-its

---
+++ tools/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tools</title>
+    <link rel="icon" type="image/png" href="../favicon.png"/>
+
+    <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+
+    <div class="row" style="margin-bottom: 2em">
+        <div class="col-md-12">
+            <h1>Tools & Web Applications</h1>
+            <p>These things are provided as is. No warranties and return policies.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <p>Create random groups from Excel and per line text lists.</p>
+            <a href="groups.html" class="btn btn-success btn-lg btn-block">Random Groups</a>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <p>Create Post-It like text notes in the browser.</p>
+            <a href="postit.html" class="btn btn-success btn-lg btn-block">Post-Its</a>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <p>Fake identities based on real names ready to use.</p>
+            <a href="../identity" class="btn btn-success btn-lg btn-block">Identities</a>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <p>Random password generator with approximate strength.</p>
+            <a href="../pwd" class="btn btn-success btn-lg btn-block">Passwords</a>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>

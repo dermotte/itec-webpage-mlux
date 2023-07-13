5522e9cb7c0e13addd705bf2dede9f8322ee3113
Mathias Lux <mlux@itec.aau.at>
2023-07-13 10:55:10 +0200

    added new course pages

--- inc/header_b.php
+++ inc/header_b.php
@@ -74,11 +74,10 @@ if (array_key_exists("id", $_GET)) {
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Quick Links<span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="index.php?id=courses/games-ss22.md">VC Computer Games SS 2022</a></li>
-                        <li><a href="index.php?id=courses/intro-cg-ss22.md">Introduction to Computer Graphics SS 2022</a></li>
-                        <li><a href="index.php?id=courses/topics-cg-ss22.md">Advanced Topics in Computer Graphics SS 2022</a></li>
+                        <li><a href="index.php?id=courses/topics-cg-ws23.md">Advanced Topics in Computer Graphics WS 2023/2024</a></li>
 <!--                        <li><a href="index.php?id=courses/esop18">VO ESOP WS 2018/2019</a></li>-->
 <!--                        <li><a href="index.php?id=courses/gameengineering-ws2122.md">Game Engineering WS 2021/2022</a></li> -->
+                        <li><a href="index.php?id=courses/gameengineering-ws2324.md">Game Engineering WS 2023/2024</a></li>
 <!--                        <li><a href="index.php?id=courses/seriousgames-ws2122.md">Non Entertainment Games WS 2021/2022</a></li> -->
 <!--                        <li role="separator" class="divider"></li>-->
 <!--                        <li><a href="index.php?id=courses/mmisss17">Multimedia Information Systems SS 2017</a></li>-->
